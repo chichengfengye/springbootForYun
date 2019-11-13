76f87b0084791a6cdaca1098fe710131ed809138
chichengfengye <1639700823@qq.com>
2019-11-13 18:49:29 +0800

    add the header console for time api

--- src/main/java/com/imooc/demo/controller/api/ApiTestController.java
+++ src/main/java/com/imooc/demo/controller/api/ApiTestController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Enumeration;
 
 @RestController
 @RequestMapping("/api")
@@ -24,7 +26,14 @@ public class ApiTestController {
     }
 
     @RequestMapping("/time")
-    public String time() {
+    public String time(HttpServletRequest request) {
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String headerName = headers.nextElement();
+            String value = request.getHeader(headerName);
+            System.out.println("========HEADER: " + headerName+"="+value);
+        }
         return dateFormat.format(new Date());
     }
+
 }
